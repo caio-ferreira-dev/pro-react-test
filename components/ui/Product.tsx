3d074bde1c792e53280706ebf89ffff32d864bea
caio-ferreira-dev <caioferreirasdj@gmail.com>
2024-05-27 18:30:26 -0300

    fix(changed to img instead of next/image)

--- components/ui/Product.tsx
+++ components/ui/Product.tsx
@@ -1,6 +1,8 @@
 import styles from "@/styles/components/ui/product.module.css";
 import Image from "next/image";
 import AddToCartButton from "./buttons/addToCartButton";
+import { useContext } from "react";
+import { DeviceWidthContext } from "../../context/DeviceWidthContext";
 
 interface ProductProps {
     title: string
@@ -13,6 +15,13 @@ interface ProductProps {
 }
 
 export default function Product({title, price, rating, image}: ProductProps) {
+    const context = useContext(DeviceWidthContext)
+
+    if (!context) {
+      throw new Error('ExampleComponent must be used within a DeviceWidthProvider');
+    }
+
+    const { isMobile } = context
 
     function renderRate(starRating: number) {
         let starsArray = [1, 2, 3, 4, 5]
@@ -21,22 +30,38 @@ export default function Product({title, price, rating, image}: ProductProps) {
         })
     }
 
+    function renderDesktop() {
+        return (
+            <>
+                <div className={styles.imageWrapper}>
+                    <img className={styles.productImage} src={image} alt="product image" />
+                </div>
+                <div className={styles.productInfos}>
+                    <h2>{title}</h2>
+                    <p className={styles.price}>$ {price.toFixed(2)}</p>
+                    <div className={styles.rating}>
+                        <div className={styles.stars}>
+                            {renderRate(rating.rate)}
+                        </div>
+                        <p className={styles.count}>{rating.count}</p>
+                    </div>
+                </div>
+                <AddToCartButton />
+            </>
+        )
+    }
+
+    function renderMobile() {
+        return (
+            <>
+            
+            </>
+        )
+    }
+
     return (
         <div className={styles.productContainer}>
-            <div className={styles.imageWrapper}>
-                <Image className={styles.productImage} src={image} alt="product image" layout="fill" objectFit="contain"></Image>
-            </div>
-            <div className={styles.productInfos}>
-                <h2>{title}</h2>
-                <p className={styles.price}>$ {price.toFixed(2)}</p>
-                <div className={styles.rating}>
-                    <div className={styles.stars}>
-                        {renderRate(rating.rate)}
-                    </div>
-                    <p className={styles.count}>{rating.count}</p>
-                </div>
-            </div>
-            <AddToCartButton />
+            {isMobile ? renderMobile() : renderDesktop()}
         </div>
     )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'fakestoreapi.com',
-        port: '',
-        pathname: '/img/**',
-      },
-    ],
-  },
 };
-
-// fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg
 
 export default nextConfig;
