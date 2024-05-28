f3b031b6042015991566dc29a3cb7dc8c5ddc7d9
caio-ferreira-dev <caioferreirasdj@gmail.com>
2024-05-28 11:57:56 -0300

    updated(header responsiveness)

--- components/ui/buttons/CartButton.tsx
+++ components/ui/buttons/CartButton.tsx
@@ -1,7 +1,9 @@
+import styles from "@/styles/components/ui/buttons/cartButton.module.css";
+
 export default function CartButton() {
     return (
         <button>
-            <img src="/cart_white.png" alt="shopping-cart" width={40} height={40}/>
+            <img className={styles.cartButtonImage} src="/cart_white.png" alt="shopping-cart" width={40} height={40}/>
         </button>
     )
 }
